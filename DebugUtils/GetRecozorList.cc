bb996ec238b1f8572f6e21a2ac324fd616231052
Josh <jccporter@gmail.com>
2022-09-06 19:56:18 +0200

    God knows, it's root

--- DebugUtils/GetRecozorList.cc
+++ DebugUtils/GetRecozorList.cc
@@ -3,58 +3,22 @@
 #include <fstream>
 #include "TTree.h"
 #include "TFile.h"
+#include "RecozorTypes.h"
 
 // run using $> root 'ampVsTime.cc("[datafile name]")'
 
-#ifdef __CLING__
-#pragma link C++ class pe_data;
-//#pragma link C++ class std::vector<pe_data>;
-#pragma link C++ class sipm_data;
-//#pragma link C++ class pmt_data;
-//#pragma link C++ class std::vector<sipm_data>;
-//#pragma link C++ class event_data;
-//#pragma link C++ class event_data_v2;
-#endif
-
-
-typedef struct {
-	float amplitude;
-	float amplitude_error;
-	// float npe;
-	// float npe_error;
-	float time;
-	float time_error;
-	
-	// for debugging purpose
-	float found_amplitude;
-	float found_time;
-} pe_data;
-
-typedef struct {
-	unsigned short ch; // channel_id
-	unsigned short id; // sipm_id
-	float chi2ndf;
-	float baseline;
-	std::vector<pe_data> pes;
-} sipm_data ;
-
-//typedef struct {
-//	float amplitude;
-//	float time;
-//} pmt_data ;
-//
-//typedef struct {
-//	unsigned int event_id;
-//	double time;
-//	double delta_time;
-//	std::vector<sipm_data> sipm;
-//	pmt_data pmt;
-//	pmt_data top_veto;
-//	pmt_data bottom_veto;
-//} event_data_v2 ;
 
 void GetRecozorList(std::string filename)
 {
+
+#ifdef CINT
+#pragma link C++ class pe_data+;
+#pragma link C++ class sipm_data+;
+#pragma link C++ class std::vector<sipm_data>+;
+#pragma link C++ class event_data_v2+;
+#endif
+
+
 //	gInterpreter->GenerateDictionary("std::vector<pe_data>", "vector");
 //	gInterpreter->GenerateDictionary("std::vector<sipm_data>", "vector");
 	
