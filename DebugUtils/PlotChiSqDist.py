f799513ff386075cd4da9bb2f432c9ca6276dd44
Josh <jccporter@gmail.com>
2022-12-19 23:45:46 +0000

    Some performance tweaks

--- DebugUtils/PlotChiSqDist.py
+++ DebugUtils/PlotChiSqDist.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 
 from DebugUtils.GenerateRMFitView import makeWaveformArray
 from DebugUtils.RecoMoreReader import readRecoMore, readWCWaveforms
@@ -15,11 +14,16 @@ class RecoMoreFitExaminer:
             self.rawWFs = readWCWaveforms(rawFile)
 
         self.reducedChiSqs = []
+        self.amps = []
+        self.times = []
         for event_ in self.RMPEs:
             if len(event_.PEData) > 0:
                 self.reducedChiSqs.append(event_.redChiSq)
+                for PE in event_.PEData:
+                    self.amps.append(PE.amplitude)
+                    self.times.append(PE.time)
 
-    def getEventPair(self, eventNumber: int, channelNumber):
+    def getEventPair(self, eventNumber: int, channelNumber: int):
         RMEvent = None
         rawEvent = None
         for event_ in self.RMPEs:
@@ -32,7 +36,7 @@ class RecoMoreFitExaminer:
 
         return RMEvent, rawEvent
 
-    def plotSingleEvent(self, eventNumber: int, channelNumber):
+    def plotSingleEvent(self, eventNumber: int, channelNumber: int):
         RMEvent_, rawEvent_ = self.getEventPair(eventNumber, channelNumber)
         xs, ys = makeWaveformArray(rawEvent_, RMEvent_)
         rawEvent_.xVals = np.array(rawEvent_.xVals)
@@ -43,10 +47,7 @@ class RecoMoreFitExaminer:
         if len(RMEvent_.PEData) > 0:
             formattedPEList += "\nAmp, time:"
         for idx, PE in enumerate(RMEvent_.PEData):
-            # if idx != len(RMEvent_.PEData):
             formattedPEList += "\n{:.4f}, {:.3f}".format(PE.amplitude, PE.time)
-            # else:
-            #     formattedPEList += "{:.4f}, {:.3f}".format(PE.amplitude, PE.time)
 
         plt.plot(xs, ys, color='k', label='Fit:\nReduced ChiSq {:.6f}'
                                           '\nBaseline: {:.5f}'
@@ -57,9 +58,24 @@ class RecoMoreFitExaminer:
         plt.show()
 
     def plotAllEvents(self):
-        eventChannelNumbers = [(event_.eventNumber, event_.channelNumber) for event_ in self.RMPEs]
+        eventChannelNumbers = [(event_.eventNumber, event_.channelNumber) for event_ in self.RMPEs if len(event_.PEData) > 60]
         for event_ in eventChannelNumbers:
             self.plotSingleEvent(event_[0], event_[1])
+
+    def plotAmps(self):
+        plt.hist(self.amps, bins=1000)
+        plt.xlabel("PE amplitudes (V)")
+        plt.show()
+
+    def plotTimes(self):
+        plt.hist(self.times, bins=1000)
+        plt.xlabel("PE times (ns)")
+        plt.show()
+
+    def plotChiSq(self):
+        plt.hist(self.reducedChiSqs, bins=1000)
+        plt.xlabel(r"$\chi^2_r$")
+        plt.show()
 
 
 if __name__ == "__main__":
@@ -68,3 +84,6 @@ if __name__ == "__main__":
 
     examiner = RecoMoreFitExaminer(recoMoreDataPath=recoMoreFileName, rawDataPath=rawFileName)
     examiner.plotAllEvents()
+    # examiner.plotAmps()
+    # examiner.plotTimes()
+    # examiner.plotChiSq()
